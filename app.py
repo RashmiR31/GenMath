5a7c82844a0a0ef1eb043154f95f31b0bd34b98f
Rashmi R <rashmir3107@gmail.com>
2024-07-18 19:38:50 +0530

    added 3 more subjects

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ import streamlit as st
 from genmath import generate_question_paper
 
 st.set_page_config(page_title="GenMath",page_icon=":books:")
-st.title("Mathematics Question Paper Generator")
+
+
+# sidebar to select the subject
+with st.sidebar:
+    subjects_available = ["Mathematics","Physics","Chemistry","Biology"]
+    subject = st.radio(label="Choose your subject",options=subjects_available)
+
+if subject:
+    st.title(subject+" Question Paper Generator")
 
 # form for the question paper details
 with st.form(key="question_paper_form"):
@@ -13,7 +21,7 @@ with st.form(key="question_paper_form"):
 
     difficulty_level = st.radio("Select Difficulty level",("Easy","Medium","Hard","Toughest","Challenging"))
 
-    question_pattern_options = ["MCQ","Reasoning & ASsertion", "Fill in the blanks","Short Answer","Word problems","Numericals"]
+    question_pattern_options = ["MCQ","Reasoning & ASsertion", "Fill in the blanks","Short Answer","Word problems","Numericals","Long Answer"]
     question_pattern = st.selectbox("Select question pattern",question_pattern_options)
 
     number_of_questions = st.number_input("Number of Questions", min_value=1, max_value=100, value=10)
@@ -22,7 +30,7 @@ with st.form(key="question_paper_form"):
 
 if submit_button:
     st.write("### Question Paper ")
-    result = generate_question_paper(grade=selected_class,topic=topic,level=difficulty_level,pattern=question_pattern,no_of_questions=number_of_questions)
+    result = generate_question_paper(subject=subject,grade=selected_class,topic=topic,level=difficulty_level,pattern=question_pattern,no_of_questions=number_of_questions)
     st.write(result)
     
 
--- genmath.py
+++ genmath.py
@@ -4,15 +4,16 @@ from langchain_core.messages import HumanMessage,SystemMessage
 
 chat_llm = ChatOpenAI(openai_api_key=st.secrets["OPENAI_API_KEY"])
 
-def generate_question_paper(grade,topic,level,pattern,no_of_questions):
+def generate_question_paper(subject,grade,topic,level,pattern,no_of_questions):
     # -------------- Create a chat model ---------------
     # create a system message
-    instructions = SystemMessage(content="""
-    You are a high school and college math teacher. You are supposed to pic class 8,9,10,11 and 12 math questions and create a question paper for students exam. Also provide solutions in detailed format for the teacher.
+    instructions = SystemMessage(content=f"""
+    You are a high school and college {subject} teacher. You are supposed to pick class 8,9,10,11 and 12 math questions and create a question paper for students exam. Also provide solutions in detailed format for the teacher.
+    Also try to include questions from previous year papers.
     """)
     
     # create a human message
-    question = HumanMessage(content=f"create math question paper with following details class {grade}, topic = {topic}, Level = {level}, pattern = {pattern}, number of questions = {no_of_questions}")
+    question = HumanMessage(content=f"create {subject} question paper with following details class {grade}, topic = {topic}, Level = {level}, pattern = {pattern}, number of questions = {no_of_questions}")
 
     # call the chat model
     response = chat_llm.invoke([
